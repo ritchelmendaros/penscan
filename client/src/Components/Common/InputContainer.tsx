50db041994147ed44483052ca3a7eeeccc613277
ritchelmendaros <ritchel.mendaros@cit.edu>
2024-08-04 03:01:51 +0800

    update: InputContainer to handle value and onChange props

--- client/src/Components/Common/InputContainer.tsx
+++ client/src/Components/Common/InputContainer.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
 
@@ -6,17 +6,26 @@ interface InputContainerProps {
     icon: IconProp;
     type?: string;
     placeholder: string;
+    value: string;
+    onChange: (e: ChangeEvent<HTMLInputElement>) => void;
 }
 
 const InputContainer: React.FC<InputContainerProps> = ({
     icon,
     type,
     placeholder,
+    value,
+    onChange,
 }) => {
     return (
         <div className='InputContainer'>
             <FontAwesomeIcon icon={icon} className='icon' />
-            <input type={type ? type : 'text'} placeholder={placeholder} />
+            <input
+                type={type}
+                placeholder={placeholder}
+                value={value}
+                onChange={onChange}
+            />
         </div>
     );
 };
