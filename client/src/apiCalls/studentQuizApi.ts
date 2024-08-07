ac98a0538c526a491833902f1c7ea12d66bde0c0
ritchelmendaros <ritchel.mendaros@cit.edu>
2024-08-07 19:18:19 +0800

    added integration for upload quiz files

---
+++ client/src/apiCalls/studentQuizApi.ts
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import { toast } from 'react-toastify';
+
+const BASE_URL = 'http://localhost:8080/api/studentquiz';
+
+export const uploadStudentQuiz = async (quizid: string, selectedFile: File) => {
+    const formData = new FormData();
+    formData.append('quizid', quizid);
+    formData.append('image', selectedFile);
+
+    try {
+        const response = await axios.post(`${BASE_URL}/upload`, formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data',
+            },
+        });
+        toast.success(response.data);
+        return response.data; 
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            const errorMessage = error.response?.data || 'An error occurred';
+            toast.error(errorMessage);
+        } else {
+            toast.error('An unexpected error occurred');
+        }
+        throw error;
+    }
+};
