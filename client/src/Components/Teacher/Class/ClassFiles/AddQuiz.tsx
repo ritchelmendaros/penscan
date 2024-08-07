ab3bc08086b7b49ad29bdc1306b7addeeb1b8d48
ritchelmendaros <ritchel.mendaros@cit.edu>
2024-08-07 13:26:47 +0800

    replaced image field to textarea in add quiz

--- client/src/Components/Teacher/Class/ClassFiles/AddQuiz.tsx
+++ client/src/Components/Teacher/Class/ClassFiles/AddQuiz.tsx
@@ -1,48 +1,59 @@
-import { useState } from 'react';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
-import Gradients from '../../../Common/Gradients';
-import Header from '../../../Common/Header';
-import InputContainer from '../../../Common/InputContainer';
-import BtnWithRobot from '../../../Common/BtnWithRobot';
-import { Link } from 'react-router-dom';
+import { useState } from "react";
+import { faClipboardList } from "@fortawesome/free-solid-svg-icons";
+import Gradients from "../../../Common/Gradients";
+import Header from "../../../Common/Header";
+import InputContainer from "../../../Common/InputContainer";
+import BtnWithRobot from "../../../Common/BtnWithRobot";
+import { Link } from "react-router-dom";
 
 const AddQuiz = () => {
+  const [quizName, setQuizName] = useState("");
+  const [answerKey, setAnswerKey] = useState("");
 
-    const [quizName, setQuizName] = useState('');
+  const handleQuizNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setQuizName(e.target.value);
+  };
 
-    const handleQuizNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setQuizName(e.target.value); 
-    };
+  const handleAnswerKeyChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setAnswerKey(e.target.value);
+  };
 
-    return (
-        <div className='AddQuiz Main MainContent'>
-            <Header />
-            <main>
-                <div className='content'>
-                    <h2>Add Quiz</h2>
-                    <InputContainer
-                        icon={faEnvelope}
-                        placeholder={'Quiz Name'}
-                        value={quizName} 
-                        onChange={handleQuizNameChange}
-                    />
+  return (
+    <div className="AddQuiz Main MainContent">
+      <Header />
+      <main>
+        <div className="content">
+          <h2>Add Quiz</h2>
+          <InputContainer
+            icon={faClipboardList}
+            placeholder={"Quiz Name"}
+            value={quizName}
+            onChange={handleQuizNameChange}
+          />
 
-                    <form action='' onSubmit={(e) => e.preventDefault()}>
-                        <div className='file-input-container'>
-                            <label htmlFor=''>Quiz answer key</label>
-                            <input type='file' />
-                        </div>
+          <form action="" onSubmit={(e) => e.preventDefault()}>
+            <div className="input-container">
+              <textarea
+                id="answerKey"
+                className="InputContainer"
+                value={answerKey}
+                onChange={handleAnswerKeyChange}
+                placeholder="Enter answer key"
+                rows={10}
+                style={{ resize: "vertical" }}
+              />
+            </div>
 
-                        <Link to={'/dashboard/class/'}>
-                            <BtnWithRobot name={'Add'} />
-                        </Link>
-                    </form>
-                </div>
-            </main>
-
-            <Gradients />
+            <Link to={"/dashboard/class/"}>
+              <BtnWithRobot name={"Add"} />
+            </Link>
+          </form>
         </div>
-    );
+      </main>
+
+      <Gradients />
+    </div>
+  );
 };
 
 export default AddQuiz;
