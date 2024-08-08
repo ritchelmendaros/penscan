fadcbe0aff8f4c54ba2af871077925d9372e1a99
arnieMYT <arnie.yurag@module-zero.com>
2024-08-08 08:03:26 +0800

    update: student class and quiz loader

--- client/src/Components/Dashboard/StudentDashboard.tsx
+++ client/src/Components/Dashboard/StudentDashboard.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Thumbnail from '../Common/Thumbnail';
 import { ClassInterface } from '../Interface/ClassInterface';
 import { useClass } from '../Context/ClassContext';
+import { SyncLoader } from 'react-spinners';
 
 interface StudentDashboardProps {
     classes: ClassInterface[];
@@ -10,6 +11,13 @@ interface StudentDashboardProps {
 
 const StudentDashboard: React.FC<StudentDashboardProps> = ({ classes }) => {
     const { setClass } = useClass();
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        if (classes) {
+            setLoading(false);
+        }
+    }, [classes]);
 
     return (
         <div className='StudentDashboard MainContent'>
@@ -18,22 +26,27 @@ const StudentDashboard: React.FC<StudentDashboardProps> = ({ classes }) => {
             </div>
 
             <div>
-                <ul className='classes'>
-                    {classes.length > 0 ? (
-                        classes.map((item, i) => (
-                            <Link to={`/dashboard/class/${item.classid}`} key={i}>
-                                <li onClick={() => setClass(item)}>
-                                    <Thumbnail name={item.classname} />
-                                    {/* <div>{item.classname}</div> */}
-                                </li>
-                            </Link>
-                        ))
-                    ) : (
-                        <h1 className='empty-state'>
-                            You have not enrolled in any classes yet.
-                        </h1>
-                    )}
-                </ul>
+                {loading ? (
+                    <div className='loader'>
+                        <SyncLoader color="#416edf" />
+                    </div>
+                ) : (
+                    <ul className='classes'>
+                        {classes.length > 0 ? (
+                            classes.map((item, i) => (
+                                <Link to={`/dashboard/class/${item.classid}`} key={i}>
+                                    <li onClick={() => setClass(item)}>
+                                        <Thumbnail name={item.classname} />
+                                    </li>
+                                </Link>
+                            ))
+                        ) : (
+                            <h1 className='empty-state'>
+                                You have not enrolled in any classes yet.
+                            </h1>
+                        )}
+                    </ul>
+                )}
             </div>
         </div>
     );
--- client/src/Components/Student/Class/ClassFiles.tsx
+++ client/src/Components/Student/Class/ClassFiles.tsx
@@ -6,11 +6,13 @@ import { Quizzes } from '../../Interface/Quiz';
 import { getQuizNamesByUserIdAndClassId, getAnswerKey } from '../../../apiCalls/QuizAPIs';
 import { Link, useNavigate } from 'react-router-dom';
 import { useQuiz } from '../../../Components/Context/QuizContext';
+import { SyncLoader } from 'react-spinners';
 
 
 const ClassFiles = () => {
     const navigate = useNavigate();
     const [quizzes, setQuizzes] = useState<Quizzes[]>([]);
+    const [loading, setLoading] = useState(true);
 
     const { clickedClass } = useClass();
     const { user } = useCurrUser();
@@ -24,10 +26,12 @@ const ClassFiles = () => {
             getQuizNamesByUserIdAndClassId(user.userid, clickedClass.classid)
                 .then((quiz) => {
                     setQuizzes(quiz);
-                    console.log(quiz);
+                    setLoading(false);
+                    // console.log(quiz);
                 })
                 .catch((err) => {
                     console.error(err);
+                    setLoading(false);
                 });
         }
     }, [clickedClass, user]);
@@ -39,20 +43,25 @@ const ClassFiles = () => {
 
     return (
         <div className='Classes'>
-            <ul>
-                {quizzes.length > 0 ? (
-                    quizzes.map((quiz, i) => (
-                        <li onClick={() => handleClick(quiz)} key={i}>
-                            <Thumbnail name={quiz.quizName}/>
-                            {/* <h2>{quiz.quizName}</h2> */}
-                        </li>
-                    ))
-                ) : (
-                    <h1 className='empty-state'>
-                        There are no quizzes in this class yet.
-                    </h1>
-                )}
-            </ul>
+            {loading ? (
+                <div className='loader'>
+                    <SyncLoader color='#416edf'/>
+                </div>
+            ) : (
+                <ul>
+                    {quizzes.length > 0 ? (
+                        quizzes.map((quiz, i) => (
+                            <li onClick={() => handleClick(quiz)} key={i}>
+                                <Thumbnail name={quiz.quizName} />
+                            </li>
+                        ))
+                    ) : (
+                        <h1 className='empty-state'>
+                            There are no quizzes in this class yet.
+                        </h1>
+                    )}
+                </ul>
+            )}
         </div>
     );
 };
