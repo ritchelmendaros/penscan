2e99033592d727ef6e7fd0e72b173f8efedc4eb0
ritchelmendaros <ritchel.mendaros@cit.edu>
2024-08-08 09:27:31 +0800

    added integration for student quiz result

--- client/src/Components/Student/Class/ClassFiles.tsx
+++ client/src/Components/Student/Class/ClassFiles.tsx
@@ -1,69 +1,79 @@
-import { useEffect, useState } from 'react';
-import Thumbnail from '../../../Components/Common/Thumbnail';
-import { useClass } from '../../../Components/Context/ClassContext';
-import { useCurrUser } from '../../../Components/Context/UserContext';
-import { Quizzes } from '../../Interface/Quiz';
-import { getQuizNamesByUserIdAndClassId, getAnswerKey } from '../../../apiCalls/QuizAPIs';
-import { Link, useNavigate } from 'react-router-dom';
-import { useQuiz } from '../../../Components/Context/QuizContext';
-import { SyncLoader } from 'react-spinners';
-
+import { useEffect, useState } from "react";
+import Thumbnail from "../../../Components/Common/Thumbnail";
+import { useClass } from "../../../Components/Context/ClassContext";
+import { useCurrUser } from "../../../Components/Context/UserContext";
+import { Quizzes, Quiz } from "../../Interface/Quiz";
+import {
+  getQuizNamesByUserIdAndClassId
+} from "../../../apiCalls/QuizAPIs";
+import { Link, useNavigate } from "react-router-dom";
+import { useQuiz } from "../../../Components/Context/QuizContext";
+import { SyncLoader } from "react-spinners";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const ClassFiles = () => {
-    const navigate = useNavigate();
-    const [quizzes, setQuizzes] = useState<Quizzes[]>([]);
-    const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
+  const [quizzes, setQuizzes] = useState<Quizzes[]>([]);
+  const [loading, setLoading] = useState(true);
 
-    const { clickedClass } = useClass();
-    const { user } = useCurrUser();
-    const { setSelectedQuiz } = useQuiz();
+  const { clickedClass } = useClass();
+  const { user } = useCurrUser();
+  const { setSelectedQuiz } = useQuiz();
 
-    useEffect(() => {
-        console.log(clickedClass?.classid);
-        console.log(user?.userid);
+  useEffect(() => {
 
-        if (user?.userid && clickedClass?.classid) {
-            getQuizNamesByUserIdAndClassId(user.userid, clickedClass.classid)
-                .then((quiz) => {
-                    setQuizzes(quiz);
-                    setLoading(false);
-                    // console.log(quiz);
-                })
-                .catch((err) => {
-                    console.error(err);
-                    setLoading(false);
-                });
-        }
-    }, [clickedClass, user]);
+    if (user?.userid && clickedClass?.classid) {
+      getQuizNamesByUserIdAndClassId(user.userid, clickedClass.classid)
+        .then((quiz) => {
+          setQuizzes(quiz);
+          setLoading(false);
+        })
+        .catch((err) => {
+          toast.error("Error fetching data", err)
+          setLoading(false);
+        });
+    }
+  }, [clickedClass, user]);
 
-    const handleClick = (quiz: Quizzes) => {
-        setSelectedQuiz(quiz);
-        navigate('/dashboard/class/quiz/quiz-result');
-    };
+  const mapQuizzesToQuiz = (quiz: Quizzes): Quiz => ({
+    quizid: quiz.quizId,
+    classid: clickedClass?.classid || "",
+    quizname: quiz.quizName,
+    teacherid: user?.userid || "",
+    quizanswerkey: "",
+  });
 
-    return (
-        <div className='Classes'>
-            {loading ? (
-                <div className='loader'>
-                    <SyncLoader color='#416edf'/>
-                </div>
-            ) : (
-                <ul>
-                    {quizzes.length > 0 ? (
-                        quizzes.map((quiz, i) => (
-                            <li onClick={() => handleClick(quiz)} key={i}>
-                                <Thumbnail name={quiz.quizName} />
-                            </li>
-                        ))
-                    ) : (
-                        <h1 className='empty-state'>
-                            There are no quizzes in this class yet.
-                        </h1>
-                    )}
-                </ul>
-            )}
+  const handleClick = (quiz: Quizzes) => {
+    const selectedQuiz = mapQuizzesToQuiz(quiz);
+    setSelectedQuiz(selectedQuiz);
+    navigate("/dashboard/class/quiz/quiz-result");
+  };
+
+  return (
+    <div className="Classes">
+      {loading ? (
+        <div className="loader">
+          <SyncLoader color="#416edf" />
         </div>
-    );
+      ) : (
+        <ul>
+          {quizzes.length > 0 ? (
+            quizzes.map((quiz, i) => (
+              <li onClick={() => handleClick(quiz)} key={i}>
+                <Thumbnail name={quiz.quizName} />
+              </li>
+            ))
+          ) : (
+            <h1 className="empty-state">
+              There are no quizzes in this class yet.
+            </h1>
+          )}
+        </ul>
+      )}
+      <ToastContainer/>
+    </div>
+  );
 };
 
 export default ClassFiles;
--- client/src/Components/Student/Quiz/QuizResult.tsx
+++ client/src/Components/Student/Quiz/QuizResult.tsx
@@ -1,102 +1,105 @@
-import Header from '../../Common/Header';
-import Gradients from '../../Common/Gradients';
-import SmilingRobot from '../../Common/SmilingRobot';
-import { useQuiz } from '../../Context/QuizContext';
-import { useEffect, useState } from 'react';
-import { getQuizResults } from '../../../apiCalls/QuizAPIs';
-import { StudentImageResult } from '../../Interface/Quiz';
-
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import Header from "../../Common/Header";
+import Gradients from "../../Common/Gradients";
+import SmilingRobot from "../../Common/SmilingRobot";
+import { useQuiz } from "../../Context/QuizContext";
+import { useEffect, useState } from "react";
+import { getQuizResults, getAnswerKey } from "../../../apiCalls/QuizAPIs";
+import { StudentImageResult } from "../../Interface/Quiz";
+import { useCurrUser } from "../../Context/UserContext";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const StudentQuizResults = () => {
-    const [answers, setAnswers] = useState<string[]>([]);
-    const [studentAnswers, setStudentAnswers] = useState<string[]>([]);
+  const [answers, setAnswers] = useState<string[]>([]);
+  const [studentAnswers, setStudentAnswers] = useState<string[]>([]);
+  const { user } = useCurrUser();
 
-    const { selectedStudentResult, selectedQuiz } = useQuiz();
-    const [studentResult, setStudentResult] = useState<StudentImageResult>();
+  const { selectedStudentResult, selectedQuiz } = useQuiz();
+  const [studentResult, setStudentResult] = useState<StudentImageResult>();
 
-    useEffect(() => {
-        if (selectedStudentResult?.userId && selectedQuiz?.quizid) {
-            getQuizResults(selectedStudentResult.userId, selectedQuiz.quizid)
-                .then((result) => {
-                    setStudentResult(result);
-                })
-                .catch((error) => {
-                    console.error(error);
-                });
-        }
-    }, [selectedStudentResult, selectedQuiz]);
+  useEffect(() => {
+    if (user?.userid && selectedQuiz?.quizid) {
+      getQuizResults(user?.userid, selectedQuiz.quizid)
+        .then((result) => {
+          setStudentResult(result);
+          return getAnswerKey(selectedQuiz.quizid);
+        })
+        .then((answerKey) => {
+          const extractedAnswers = extractAnswers(answerKey);
+          setAnswers(extractedAnswers);
+        })
+        .catch((error) => {
+          toast.error("Error fetching data", error)
+        });
+    }
+  }, [user, selectedQuiz]);
 
-    const extractAnswers = (input: string) => {
-        return input
-            .trim()
-            .split('\n')
-            .map((line) => line.replace(/^\d+\.\s*/, ''));
-    };
+  const extractAnswers = (input: string) => {
+    return input
+      .trim()
+      .split("\n")
+      .map((line) => line.replace(/^\d+\.\s*/, ""));
+  };
 
-    useEffect(() => {
-        if (studentResult?.recognizedtext) {
-            setStudentAnswers(extractAnswers(studentResult?.recognizedtext));
-        }
+  useEffect(() => {
+    if (studentResult?.recognizedtext) {
+      setStudentAnswers(extractAnswers(studentResult.recognizedtext));
+    }
+  }, [studentResult]);
 
-        if (selectedQuiz?.quizanswerkey) {
-            const theAnswers = extractAnswers(selectedQuiz.quizanswerkey);
-            setAnswers(theAnswers);
-        }
-    }, [selectedQuiz, studentResult]);
+  console.log("Selected Student Result:", selectedStudentResult);
+  console.log("Current User:", user);
 
-    return (
-        <div className='QuizResults Main MainContent'>
-            <Header />
-            <main>
-                <div className='student-details'>
-                    <div>
-                        <h1>
-                            {selectedStudentResult?.firstName}{' '}
-                            {selectedStudentResult?.lastName}
-                        </h1>
-                        <h1>Score: {selectedStudentResult?.score}</h1>
-                    </div>
-                </div>
-
-                <div className='main-results'>
-                    <img
-                        src={`data:image/png;base64,${studentResult?.base64Image}`}
-                        alt=''
-                    />
-
-                    <div className='table'>
-                        <ul className='thead'>
-                            <li className='th'>
-                                <p />
-                                <p className='td'>Item No.</p>
-                                <p className='td'>Scanned Answer</p>
-                                <p className='td'>Correct Answer</p>
-                                <p />
-                            </li>
-                        </ul>
-                        <ul className='tbody'>
-                            {answers.map((item, i) => (
-                                <li key={i} className='tr'>
-                                    <p className='td'></p>
-                                    <p className='td'>{i + 1}</p>
-                                    <p className='td'>
-                                        {studentAnswers[i + 1]}
-                                    </p>
-                                    <p className='td'>{item}</p>
-                                    <p className='td'></p>
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
-                </div>
-            </main>
-
-            <SmilingRobot />
-            <Gradients />
+  return (
+    <div className="QuizResults Main MainContent">
+      <Header />
+      <main>
+        <div className="student-details">
+          <div>
+            <h3>
+              {user?.firstname}{" "}
+              {user?.lastname}
+            </h3>
+          </div>
+          <h3>Score: {studentResult?.score}</h3>
         </div>
-    );
+
+        <div className="main-results">
+          <img
+            src={`data:image/png;base64,${studentResult?.base64Image}`}
+            alt=""
+          />
+
+          <div className="table">
+            <ul className="thead">
+              <li className="th">
+                <p />
+                <p className="td">Item No.</p>
+                <p className="td">Scanned Answer</p>
+                <p className="td">Correct Answer</p>
+                <p />
+              </li>
+            </ul>
+            <ul className="tbody">
+              {answers.map((item, i) => (
+                <li key={i} className="tr">
+                  <p className="td"></p>
+                  <p className="td">{i + 1}</p>
+                  <p className="td">{studentAnswers[i] || "No answer"}</p>
+                  <p className="td">{item}</p>
+                  <p className="td"></p>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </main>
+
+      <SmilingRobot />
+      <Gradients />
+      <ToastContainer/>
+    </div>
+  );
 };
 
 export default StudentQuizResults;
