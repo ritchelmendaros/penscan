c2b1578f64cfa8ef87826e792a36b82610d8b721
ArnieYurag <127393398+ArnieYurag@users.noreply.github.com>
2024-11-18 02:09:43 +0800

    make search dynamic

--- client/src/Components/Teacher/Class/ClassStudents/AddStudent.tsx
+++ client/src/Components/Teacher/Class/ClassStudents/AddStudent.tsx
@@ -1,15 +1,11 @@
-import { useState, useEffect } from "react";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import Header from "../../../Common/Header";
 import BtnWithRobot from "../../../Common/BtnWithRobot";
 import Gradients from "../../../Common/Gradients";
 import DropdownContainer from "../../../Common/DropdownContainer";
-import { fetchAllStudents } from "../../../../apiCalls/userApi";
-import { addStudentToClass } from "../../../../apiCalls/studentApi";
-import { useClass } from "../../../Context/ClassContext";
-import { useNavigate } from "react-router-dom";
-import { toast, ToastContainer } from 'react-toastify'; 
-import 'react-toastify/dist/ReactToastify.css';
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Search from "./Component/Search";
 
 interface Student {
   userid: string;
@@ -19,115 +15,43 @@ interface Student {
 }
 
 const AddStudent: React.FC = () => {
-  const [studentName, setStudentName] = useState("");
-  const [allStudents, setAllStudents] = useState<Student[]>([]);
-  const [filteredStudents, setFilteredStudents] = useState<Student[]>([]);
-  const [selectedStudent, setSelectedStudent] = useState<Student | null>(null);
-  const [showDropdown, setShowDropdown] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const { clickedClass } = useClass();
-  const classId = clickedClass?.classid;
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const fetchStudents = async () => {
-      try {
-        const studentsData = await fetchAllStudents();
-        setAllStudents(studentsData);
-        setFilteredStudents(studentsData);
-      } catch (error) {
-        toast.error("Error fetching students");
-      }
-    };
-
-    fetchStudents();
-  }, []);
-
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value;
-    setStudentName(value);
-    setShowDropdown(true);
-    setFilteredStudents(
-      allStudents.filter((student) =>
-        `${student.firstname} ${student.lastname}`
-          .toLowerCase()
-          .includes(value.toLowerCase())
-      )
-    );
-  };
-
-  const handleDropdownBlur = () => {
-
-    const username = studentName.split(" ").pop();
-    const matchedStudent = allStudents.find(
-      (student) => `${student.username}` === username
-    );
-
-    if (matchedStudent) {
-      setSelectedStudent(matchedStudent);
-    } else {
-      setSelectedStudent(null);
-    }
-
-    setShowDropdown(false);
-    console.log(showDropdown)
-  };
-
-  const handleAddStudent = async () => {
-    if (selectedStudent) {
-      if (!classId) {
-        toast.error("Class ID is missing. Please try again.");
-        return;
-      }
-      setLoading(true); 
-      try {
-        await addStudentToClass(
-          selectedStudent.userid,
-          classId
-        );
-        toast.dark("Student added successfully!"); 
-            setTimeout(() => {
-                navigate(`/dashboard/class`); 
-            }, 500);
-      } catch (error) {
-        setLoading(false);
-        toast.error("Error adding student");
-      }
-    } else {
-      toast.error("Please select a student before adding.");
-    }
-  };
-
   return (
-    <div className="AddStudent Main MainContent">
+    // <div className="AddStudent Main MainContent">
+    //   <Search/>
+    //   <Header />
+    //   <main>
+    //     <div className="content">
+    //       <h2>Add Student</h2>
+    //       <DropdownContainer
+    //         icon={faPlus}
+    //         type="text"
+    //         placeholder="Enter Student Name"
+    //         value={studentName}
+    //         onChange={handleInputChange}
+    //         onBlur={handleDropdownBlur}
+    //         list="students"
+    //       />
+    //       <datalist id="students">
+    //         {filteredStudents.map((student) => (
+    //           <option
+    //             key={student.userid}
+    //             value={`${student.firstname} ${student.lastname} - ${student.username}`}
+    //           />
+    //         ))}
+    //       </datalist>
+    //       <BtnWithRobot name={"Add"} onClick={handleAddStudent} loading={loading}/>{" "}
+    //     </div>
+    //   </main>
+
+    //   <Gradients />
+    //   <ToastContainer />
+    // </div>
+    <>
       <Header />
       <main>
-        <div className="content">
-          <h2>Add Student</h2>
-          <DropdownContainer
-            icon={faPlus}
-            type="text"
-            placeholder="Enter Student Name"
-            value={studentName}
-            onChange={handleInputChange}
-            onBlur={handleDropdownBlur}
-            list="students"
-          />
-          <datalist id="students">
-            {filteredStudents.map((student) => (
-              <option
-                key={student.userid}
-                value={`${student.firstname} ${student.lastname} - ${student.username}`}
-              />
-            ))}
-          </datalist>
-          <BtnWithRobot name={"Add"} onClick={handleAddStudent} loading={loading}/>{" "}
-        </div>
+        <Search />
       </main>
-
-      <Gradients />
-      <ToastContainer />
-    </div>
+    </>
   );
 };
 
---
+++ client/src/Components/Teacher/Class/ClassStudents/Component/Search.tsx
@@ -0,0 +1,171 @@
+import {
+  Calculator,
+  Calendar,
+  CreditCard,
+  Settings,
+  Smile,
+  User,
+} from "lucide-react";
+
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/Components/ui/command";
+
+import { useState, useEffect, useRef } from "react";
+import { fetchAllStudents } from "@/apiCalls/userApi";
+import { addStudentToClass } from "@/apiCalls/studentApi";
+import { useClass } from "@/Components/Context/ClassContext";
+import { useNavigate } from "react-router-dom";
+import { toast, ToastContainer } from "react-toastify";
+import { ScrollArea } from "@/Components/ui/scroll-area";
+
+interface Student {
+  userid: string;
+  username: string;
+  firstname: string;
+  lastname: string;
+}
+
+const Search = () => {
+  const [studentName, setStudentName] = useState("");
+  const [allStudents, setAllStudents] = useState<Student[]>([]);
+  const [filteredStudents, setFilteredStudents] = useState<Student[]>([]);
+  const [selectedStudent, setSelectedStudent] = useState<Student | null>(null);
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const { clickedClass } = useClass();
+  const classId = clickedClass?.classid;
+  const [userTyping, setUserTyping] = useState<boolean>(false);
+  const typingTimerRef = useRef<NodeJS.Timeout>();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchStudents = async () => {
+      try {
+        const studentsData = await fetchAllStudents();
+        setAllStudents(studentsData);
+        setFilteredStudents(studentsData);
+      } catch (error) {
+        toast.error("Error fetching students");
+      }
+    };
+
+    fetchStudents();
+  }, []);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setStudentName(value);
+    setShowDropdown(true);
+    setFilteredStudents(
+      allStudents.filter((student) =>
+        `${student.firstname} ${student.lastname}`
+          .toLowerCase()
+          .includes(value.toLowerCase())
+      )
+    );
+  };
+
+  const handleDropdownBlur = () => {
+    const username = studentName.split(" ").pop();
+    const matchedStudent = allStudents.find(
+      (student) => `${student.username}` === username
+    );
+
+    if (matchedStudent) {
+      setSelectedStudent(matchedStudent);
+    } else {
+      setSelectedStudent(null);
+    }
+
+    setShowDropdown(false);
+    console.log(showDropdown);
+  };
+
+  const handleAddStudent = async () => {
+    if (selectedStudent) {
+      if (!classId) {
+        toast.error("Class ID is missing. Please try again.");
+        return;
+      }
+      setLoading(true);
+      try {
+        await addStudentToClass(selectedStudent.userid, classId);
+        toast.dark("Student added successfully!");
+        setTimeout(() => {
+          navigate(`/dashboard/class`);
+        }, 500);
+      } catch (error) {
+        setLoading(false);
+        toast.error("Error adding student");
+      }
+    } else {
+      toast.error("Please select a student before adding.");
+    }
+  };
+
+  const handleValueChange = (value: string): void => {
+    setUserTyping(true);
+
+    if (typingTimerRef.current) {
+      clearTimeout(typingTimerRef.current);
+    }
+
+    typingTimerRef.current = setTimeout(() => {
+      setUserTyping(false);
+    }, 500);
+  };
+  return (
+    //       <datalist id="students">
+    //         {filteredStudents.map((student) => (
+    //           <option
+    //             key={student.userid}
+    //             value={`${student.firstname} ${student.lastname} - ${student.username}`}
+    //           />
+    //         ))}
+    //       </datalist>
+    <div className="h-screen">
+      <div className="flex justify-center items-center h-full">
+        <div className="h-[300px]">
+          <Command
+            className=" rounded-lg border shadow-md md:min-w-[450px]"
+            onValueChange={handleValueChange}
+          >
+            <CommandInput placeholder="Enter a student name" />
+            <CommandList>
+              <CommandEmpty>No results found.</CommandEmpty>
+              <CommandGroup heading="Suggestions">
+                <ScrollArea
+                  className={`${
+                    filteredStudents.length > 4 && userTyping ? "h-[100px]" : ""
+                  }`}
+                >
+                  {filteredStudents.map((student) => (
+                    <CommandItem key={student.username}>
+                      <span>
+                        {student.firstname.charAt(0).toUpperCase() +
+                          student.firstname.slice(1)}{" "}
+                        {student.lastname.charAt(0).toUpperCase() +
+                          student.lastname.slice(1)}
+                      </span>
+                      <span className="text-xs text-slate-400">
+                        {student.username}
+                      </span>
+                    </CommandItem>
+                  ))}
+                </ScrollArea>
+              </CommandGroup>
+            </CommandList>
+          </Command>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Search;
